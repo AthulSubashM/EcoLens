bd5778de0b90cb47da84273d3f0f34428ccbc2de
Ibrahim Yousufuddin <ibrahimyousufuddin@trentu.ca>
2024-11-09 13:24:25 -0500

    Added loading asset

---
+++ eco-lens/src/Components/Loading.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Loading(){
+    return(
+        <>
+            <div className="loading-container">
+                <div className="loading_image">
+                    <img src = "../Assests/loading_image.png"/>
+                </div>
+            </div>
+        </>
+    );
+}
