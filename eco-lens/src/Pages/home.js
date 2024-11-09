7081eeca872043fffe6336f1c3cd8c0760716c95
Prabal-Kochhar <126103288+Prabal-Kochhar@users.noreply.github.com>
2024-11-09 04:25:00 -0500

    change

--- eco-lens/src/Pages/home.js
+++ eco-lens/src/Pages/home.js
@@ -1,13 +1,15 @@
+import Images from '../Images/Designer.jpeg';
 import Nav from '../Components/Navbar'
 import Hero from "../Components/Hero";
 import React from 'react';
-import img from 'designer.jpeg';
+
 
 export default function Home() {
   return (
         <>
           <Nav/>
           <Hero/>
+          <img src = {Images}/>
         </>
       );
 }
