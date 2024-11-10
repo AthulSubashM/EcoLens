1e451ee0e11f13dde5d30205629493075158cc90
Ibrahim Yousufuddin <ibrahimyousufuddin@trentu.ca>
2024-11-09 19:25:59 -0500

    adding background to all body

--- eco-lens/src/Pages/home.js
+++ eco-lens/src/Pages/home.js
@@ -3,15 +3,12 @@ import Hero from "../Components/Hero";
 import ImageToText from "../LLM/ImageToText";
 import LoadingImage from "../Components/Loading";
 import React from 'react';
-import Background from "../Components/Background";
-
 
 export default function Home() {
   return (
         <>
         
         <LoadingImage/>
-          <Background/>
           <Nav/>
           <ImageToText/>
           
