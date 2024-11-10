cd7518e834512af95504e4aa862a73088b3f93d3
Athul <amarottikkal@trentu.ca>
2024-11-10 06:55:51 -0500

    footer

--- eco-lens/src/Components/Footer.js
+++ eco-lens/src/Components/Footer.js
@@ -1,11 +1,21 @@
-import React, { useEffect, useState } from 'react';
-
+import React from 'react';
+import { ReactComponent as Linkedin } from '../Assets/linkedin.svg'; // Import SVG as React component
 
 export default function Footer() {
-
     return (
         <div className='footer-container'>
-
+            <h1>Eco Lens</h1>
+            <div className='footer-links'>
+                <a href='https://www.linkedin.com/in/athulmsubash/' target='_blank' rel='noreferrer'>
+                    <Linkedin className="linkedin" /> Athul Subash
+                </a>
+                <a href='https://www.linkedin.com/in/ibrahim-yousufuddin-ab9aa4226/' target='_blank' rel='noreferrer'>
+                <Linkedin className="linkedin" /> Ibrahim Yousufuddin
+                </a>
+                <a href='https://www.linkedin.com/in/prabal-kochhar/' target='_blank' rel='noreferrer'>
+                <Linkedin className="linkedin" /> Prabal Kochhar
+                </a>
+            </div>
         </div>
     );
 }
