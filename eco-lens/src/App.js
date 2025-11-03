4059bcd5153211fe46d2ac7d1624100f01d01b1e
AthulSubashM <athul.m.subash@gmail.com>
2025-11-03 01:39:49 -0500

    Router fix

--- eco-lens/src/App.js
+++ eco-lens/src/App.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import Home from './Pages/home';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Result from './Pages/result';
-import Confirm from './Pages/confirm';
-import { ImageDataProvider } from './Contexts/ImageDataContext';
+import React from "react";
+import Home from "./Pages/home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Result from "./Pages/result";
+import Confirm from "./Pages/confirm";
+import { ImageDataProvider } from "./Contexts/ImageDataContext";
 
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/EcoLens">
       <ImageDataProvider>
-      <Routes>
+        <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/result" element={<Result />} />
           <Route path="/confirm" element={<Confirm />} />
